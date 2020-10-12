9107bfb1dad0e044500c4df0ded78ce0f43858c3
MithralSmith <TheSmith@MithralSmith>
2020-10-12 11:45:23 -0700

    Added a couple of YEntity values to the in memory database.

--- Persistence/Contexts/AppDbContext.cs
+++ Persistence/Contexts/AppDbContext.cs
@@ -29,6 +29,25 @@ namespace WebAPI.API.Persistence.Contexts
             builder.Entity<YEntity>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
             builder.Entity<YEntity>().Property(p => p.Name).IsRequired().HasMaxLength(50);
             builder.Entity<YEntity>().Property(p => p.QuantityInPackage).IsRequired();
+            builder.Entity<YEntity>().HasData
+            (
+                new YEntity
+                {
+                    Id = 100,
+                    Name = "Apple",
+                    QuantityInPackage = 1,
+                    UnitOfMeasurement = EUnitOfMeasurement.Unity,
+                    XEntityId = 100
+                },
+                new YEntity
+                {
+                    Id = 101,
+                    Name = "Milk",
+                    QuantityInPackage = 2,
+                    UnitOfMeasurement = EUnitOfMeasurement.Liter,
+                    XEntityId = 101
+                }
+            );
 
         }
     }
