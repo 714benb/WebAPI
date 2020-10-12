6c808da861276e515e70279c02015bc9b356a499
MithralSmith <TheSmith@MithralSmith>
2020-10-12 10:51:42 -0700

    Added The EnumExtensions ToDescriptionString method and set up automap to use it.

--- Mapping/ModelToResourceProfile.cs
+++ Mapping/ModelToResourceProfile.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
+using Microsoft.AspNetCore.Routing.Constraints;
 using WebAPI.API.Domain.Models;
 using WebAPI.API.Resources;
+using WebAPI.API.Extensions;
 
 namespace WebAPI.API.Mapping
 {
@@ -9,7 +11,10 @@ namespace WebAPI.API.Mapping
         public ModelToResourceProfile()
         {
             CreateMap<XEntity, XEntityResource>();
-            CreateMap<YEntity, YEntityResource>();
+            CreateMap<YEntity, YEntityResource>()
+                .ForMember(src => src.UnitOfMeasurement,
+                    opt => opt.MapFrom(
+                        src => src.UnitOfMeasurement.ToDescriptionString()));
         }
     }
 }
